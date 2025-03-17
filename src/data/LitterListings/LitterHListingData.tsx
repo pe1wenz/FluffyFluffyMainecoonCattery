91a140e7f73554486fddb5786d204420dc07cd80
pe1wenz <peiwenz@vt.edu>
2025-03-17 14:06:06 -0400

    Update litter h status -pz

--- src/data/LitterListings/LitterHListingData.tsx
+++ src/data/LitterListings/LitterHListingData.tsx
@@ -4,11 +4,21 @@ import Honeybun5w2 from "../../assets/images/litter-images/litter-h/Honeybun/pho
 import Honeybun5w3 from "../../assets/images/litter-images/litter-h/Honeybun/photos/5weeks/Honeybun3.jpeg";
 import Honeybun5w4 from "../../assets/images/litter-images/litter-h/Honeybun/photos/5weeks/Honeybun4.jpeg";
 
+import Honeybun10w1 from "../../assets/images/litter-images/litter-h/Honeybun/photos/10weeks/Honeybun1.jpeg";
+import Honeybun10w2 from "../../assets/images/litter-images/litter-h/Honeybun/photos/10weeks/Honeybun2.jpeg";
+import Honeybun10w3 from "../../assets/images/litter-images/litter-h/Honeybun/photos/10weeks/Honeybun3.jpeg";
+import Honeybun10w4 from "../../assets/images/litter-images/litter-h/Honeybun/photos/10weeks/Honeybun4.jpeg";
+
 // hazelnut photos
 import Hazelnut5w1 from "../../assets/images/litter-images/litter-h/Hazelnut/photos/5weeks/Hazelnut1.jpeg";
 import Hazelnut5w2 from "../../assets/images/litter-images/litter-h/Hazelnut/photos/5weeks/Hazelnut2.jpeg";
 import Hazelnut5w3 from "../../assets/images/litter-images/litter-h/Hazelnut/photos/5weeks/Hazelnut3.jpeg";
 import Hazelnut5w4 from "../../assets/images/litter-images/litter-h/Hazelnut/photos/5weeks/Hazelnut4.jpeg";
+
+import Hazelnut10w1 from "../../assets/images/litter-images/litter-h/Hazelnut/photos/10weeks/Hazelnut1.jpeg";
+import Hazelnut10w2 from "../../assets/images/litter-images/litter-h/Hazelnut/photos/10weeks/Hazelnut2.jpeg";
+import Hazelnut10w3 from "../../assets/images/litter-images/litter-h/Hazelnut/photos/10weeks/Hazelnut3.jpeg";
+import Hazelnut10w4 from "../../assets/images/litter-images/litter-h/Hazelnut/photos/10weeks/Hazelnut4.jpeg";
 
 // hairball photos
 import Hairball5w1 from "../../assets/images/litter-images/litter-h/Hairball/photos/5weeks/Hairball1.jpeg";
@@ -16,11 +26,23 @@ import Hairball5w2 from "../../assets/images/litter-images/litter-h/Hairball/pho
 import Hairball5w3 from "../../assets/images/litter-images/litter-h/Hairball/photos/5weeks/Hairball3.jpeg";
 import Hairball5w4 from "../../assets/images/litter-images/litter-h/Hairball/photos/5weeks/Hairball4.jpeg";
 
+import Hairball10w1 from "../../assets/images/litter-images/litter-h/Hairball/photos/10weeks/Hairball1.jpeg";
+import Hairball10w2 from "../../assets/images/litter-images/litter-h/Hairball/photos/10weeks/Hairball2.jpeg";
+import Hairball10w3 from "../../assets/images/litter-images/litter-h/Hairball/photos/10weeks/Hairball3.jpeg";
+import Hairball10w4 from "../../assets/images/litter-images/litter-h/Hairball/photos/10weeks/Hairball4.jpeg";
+import Hairball10w5 from "../../assets/images/litter-images/litter-h/Hairball/photos/10weeks/Hairball5.jpeg";
+import Hairball10w6 from "../../assets/images/litter-images/litter-h/Hairball/photos/10weeks/Hairball6.jpeg";
+
 // hufflepuff photos
 import Hufflepuff5w1 from "../../assets/images/litter-images/litter-h/Hufflepuff/photos/5weeks/Hufflepuff1.jpeg";
 import Hufflepuff5w2 from "../../assets/images/litter-images/litter-h/Hufflepuff/photos/5weeks/Hufflepuff2.jpeg";
 import Hufflepuff5w3 from "../../assets/images/litter-images/litter-h/Hufflepuff/photos/5weeks/Hufflepuff3.jpeg";
 import Hufflepuff5w4 from "../../assets/images/litter-images/litter-h/Hufflepuff/photos/5weeks/Hufflepuff4.jpeg";
+
+import Hufflepuff10w1 from "../../assets/images/litter-images/litter-h/Hufflepuff/photos/10weeks/Hufflepuff1.jpeg";
+import Hufflepuff10w2 from "../../assets/images/litter-images/litter-h/Hufflepuff/photos/10weeks/Hufflepuff2.jpeg";
+import Hufflepuff10w3 from "../../assets/images/litter-images/litter-h/Hufflepuff/photos/10weeks/Hufflepuff3.jpeg";
+import Hufflepuff10w4 from "../../assets/images/litter-images/litter-h/Hufflepuff/photos/10weeks/Hufflepuff4.jpeg";
 
 export const LitterHListingData = [
   {
@@ -32,6 +54,27 @@ export const LitterHListingData = [
     path: "honeybun",
     litterName: "H",
     images: [
+
+      {
+        original: Honeybun10w1,
+        thumbnail: Honeybun10w1,
+        comment: "10weeks",
+      },
+      {
+        original: Honeybun10w2,
+        thumbnail: Honeybun10w2,
+        comment: "10weeks",
+      },
+      {
+        original: Honeybun10w3,
+        thumbnail: Honeybun10w3,
+        comment: "10weeks",
+      },
+      {
+        original: Honeybun10w4,
+        thumbnail: Honeybun10w4,
+        comment: "10weeks",
+      },
       {
         original: Honeybun5w1,
         thumbnail: Honeybun5w1,
@@ -64,6 +107,26 @@ export const LitterHListingData = [
     litterName: "G",
     images: [
       {
+        original: Hazelnut10w1,
+        thumbnail: Hazelnut10w1,
+        comment: "10weeks",
+      },
+      {
+        original: Hazelnut10w2,
+        thumbnail: Hazelnut10w2,
+        comment: "10weeks",
+      },
+      {
+        original: Hazelnut10w3,
+        thumbnail: Hazelnut10w3,
+        comment: "10weeks",
+      },
+      {
+        original: Hazelnut10w4,
+        thumbnail: Hazelnut10w4,
+        comment: "10weeks",
+      },
+      {
         original: Hazelnut5w1,
         thumbnail: Hazelnut5w1,
         comment: "5weeks",
@@ -95,6 +158,36 @@ export const LitterHListingData = [
     litterName: "G",
     images: [
       {
+        original: Hairball10w1,
+        thumbnail: Hairball10w1,
+        comment: "10weeks",
+      },
+      {
+        original: Hairball10w2,
+        thumbnail: Hairball10w2,
+        comment: "10weeks",
+      },
+      {
+        original: Hairball10w3,
+        thumbnail: Hairball10w3,
+        comment: "10weeks",
+      },
+      {
+        original: Hairball10w4,
+        thumbnail: Hairball10w4,
+        comment: "10weeks",
+      },
+      {
+        original: Hairball10w5,
+        thumbnail: Hairball10w5,
+        comment: "10weeks",
+      },
+      {
+        original: Hairball10w6,
+        thumbnail: Hairball10w6,
+        comment: "10weeks",
+      },
+      {
         original: Hairball5w1,
         thumbnail: Hairball5w1,
         comment: "5weeks",
@@ -125,6 +218,26 @@ export const LitterHListingData = [
     path: "hufflepuff",
     litterName: "G",
     images: [
+      {
+        original: Hufflepuff10w1,
+        thumbnail: Hufflepuff10w1,
+        comment: "10weeks",
+      },
+      {
+        original: Hufflepuff10w2,
+        thumbnail: Hufflepuff10w2,
+        comment: "10weeks",
+      },
+      {
+        original: Hufflepuff10w3,
+        thumbnail: Hufflepuff10w3,
+        comment: "10weeks",
+      },
+      {
+        original: Hufflepuff10w4,
+        thumbnail: Hufflepuff10w4,
+        comment: "10weeks",
+      },
       {
         original: Hufflepuff5w1,
         thumbnail: Hufflepuff5w1,
