d8eb8d7b536b9987dc8e506852a4e18eb5bc1fa9
pe1wenz <peiwenz@vt.edu>
2025-04-29 12:27:29 -0400

    Update Litter H -pz

--- src/data/LitterListings/LitterHListingData.tsx
+++ src/data/LitterListings/LitterHListingData.tsx
@@ -229,7 +229,7 @@ export const LitterHListingData = [
     gender: "Male",
     color: "Red & White",
     birthday: "01/05/2025",
-    availability: "Reserved",
+    availability: "Sold",
     path: "hairball",
     litterName: "G",
     images: [
